19585a6f68bacebf5703834eb7b7476b4223cb87
beimnetw <beimnetworku101@gmail.com>
2026-01-14 15:20:20 +0300

    03:database setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+import { AuthConfig } from "convex/server";
+export default{
+providers:[
+    {
+        domain:process.env.CLERK_JWT_ISSUER_dOMAIN!,
+        applicationID:"convex"
+        },
+],
+
+
+}
